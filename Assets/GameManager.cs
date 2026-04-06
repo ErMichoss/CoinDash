d8ee1438e77cc239ff8d36bfb17d9ebab1e04f41
unknown <nik.lafuente@gmail.com>
2026-04-06 16:43:38 +0200

    fase 4 - menu principla, game over y botones de navegación

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,22 +8,37 @@ public class GameManager : MonoBehaviour
     public int coinCount = 0;
     public TextMeshProUGUI coinText;
     public TextMeshProUGUI resultText;
+    public GameObject endUI;
 
-    public void CoinCollected() 
+    public void CoinCollected()
     {
         coinCount++;
         coinText.text = "Monedas: " + coinCount;
     }
 
     public void Win()
-    { 
+    {
+        endUI.SetActive(true);
         resultText.text = "Success!";
         Time.timeScale = 0f;
     }
 
     public void GameOver()
     {
+        endUI.SetActive(true);
         resultText.text = "Game Over :C";
         Time.timeScale = 0f;
+    }
+
+    public void Reiniciar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void VolverAlMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
