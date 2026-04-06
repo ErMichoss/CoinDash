f298551a448f0c6d310f88ade62dac201a856764
unknown <nik.lafuente@gmail.com>
2026-04-06 15:42:25 +0200

    fase 3 - victoria, game over y zona de muerte

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,11 +6,23 @@ public class GameManager : MonoBehaviour
 
     public int coinCount = 0;
     public TextMeshProUGUI coinText;
+    public TextMeshProUGUI resultText;
 
     public void CoinCollected() 
     {
         coinCount++;
         coinText.text = "Monedas: " + coinCount;
     }
-    
+
+    public void Win()
+    { 
+        resultText.text = "Success!";
+        Time.timeScale = 0f;
+    }
+
+    public void GameOver()
+    {
+        resultText.text = "Game Over :C";
+        Time.timeScale = 0f;
+    }
 }
